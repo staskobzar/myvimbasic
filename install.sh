68f218b9b3e14c5b2473570101165f80d742b2fb
Stas Kobzar <stas@modulis.ca>
2015-06-08 09:25:41 -0400

    Replace old openser syntax with a new from OpenSIPS github repo.

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@ cd ~
 git clone https://github.com/staskobzar/myvimbasic.git
 ln -s ~/myvimbasic/vimrc ~/.vimrc
 mkdir -p ~/.vim/syntax
-cp ~/myvimbasic/openser.vim ~/.vim/syntax/
+wget -O ~/.vim/syntax/opensips.vim https://raw.githubusercontent.com/OpenSIPS/opensips/master/utils/vim/opensips.vim
+if [ $? -gt 0 ] ; then
+  cp ~/myvimbasic/opensips.vim ~/.vim/syntax/
+fi
