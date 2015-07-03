8304a47cc6ac6febb4bb2d8720174bfa19047f68
Stas Kobzar <stas@modulis.ca>
2015-07-03 10:05:58 -0400

    Fix cert check diable for wget.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cd ~
 git clone https://github.com/staskobzar/myvimbasic.git
 ln -s ~/myvimbasic/vimrc ~/.vimrc
 mkdir -p ~/.vim/syntax
-wget -O ~/.vim/syntax/opensips.vim https://raw.githubusercontent.com/OpenSIPS/opensips/master/utils/vim/opensips.vim
+wget --no-check-certificate -O ~/.vim/syntax/opensips.vim https://raw.githubusercontent.com/OpenSIPS/opensips/master/utils/vim/opensips.vim
 if [ $? -gt 0 ] ; then
   cp ~/myvimbasic/opensips.vim ~/.vim/syntax/
 fi
